14afdf872ea8cebe226e89b774398a616ccca819
mandy0207 <mandyatwork24@gmail.com>
2023-07-18 11:50:49 -0400

    fifth session

--- src/SeleniumLocators.java
+++ src/SeleniumLocators.java
@@ -4,8 +4,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumLocators {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
+		
+		/**
+		 * Here we are using following locator
+		 * 
+		 * ID, ClassName, Name, LinkText, PartialLinktext
+		 */
 		
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\msingh\\Desktop\\Selenium Session\\driver\\chromedriver.exe");
@@ -14,10 +20,29 @@ public class SeleniumLocators {
 		driver.manage().window().maximize();
 		driver.get("https://www.saucedemo.com/");
 		
-		driver.findElement(By.id("user-name")).sendKeys("myusername");
-		driver.findElement(By.name("password")).sendKeys("mypassword");
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.name("password")).sendKeys("secret_sauce");
+		driver.findElement(By.className("submit-button")).click();
+		
+		driver.findElement(By.id("react-burger-menu-btn")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("About")).click();
+		System.out.println("Title of about page="+driver.getTitle());
+		driver.navigate().back();
+		driver.findElement(By.partialLinkText("Logo")).click();
+		
+		/**
+		 * getTitle() method is used to get title of page under control of webDRiver
+		 */
+		System.out.println("Title of Login page="+driver.getTitle());
+		
+		/**
+		 * GetText() method is used to grab text inside any webelement
+		 */
+		String text = driver.findElement(By.className("login_logo")).getText();
+		System.out.println(text);
+		driver.quit();
 	
-
 	}
 
 }
