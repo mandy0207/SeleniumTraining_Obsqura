e6f9ba11a83328ad0f387ebe07581e40d5f1cc42
mandy0207 <mandyatwork24@gmail.com>
2023-07-21 11:01:39 -0400

    Session 6

--- src/UnderstandingWebElements.java
+++ src/UnderstandingWebElements.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class UnderstandingWebElements {
@@ -11,6 +16,24 @@ public class UnderstandingWebElements {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.saucedemo.com/");
+		WebElement userName = driver.findElement(By.cssSelector("#user-name"));
+		userName.sendKeys("standard_user");
+		WebElement passWord = driver.findElement(By.cssSelector("input[placeholder='Password']"));
+		passWord.sendKeys("secret_sauce");
+		WebElement loginBtn = driver.findElement(By.cssSelector(".submit-button"));
+		loginBtn.click();
+		
+		/**
+		 * How to use WebElements
+		 */
+
+		
+		List<WebElement> addToCart = driver.findElements(By.cssSelector(".btn_small.btn_inventory"));
+		System.out.println("Size of List = "+addToCart.size());
+		addToCart.get(1).click();
+//		for(int i =0;i<addToCart.size();i++) {
+//			addToCart.get(i).click();
+//		}
 
 	}
 
