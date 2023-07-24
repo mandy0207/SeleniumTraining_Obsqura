f39a49e84afac1c7612180522d190a11cd14138c
mandy0207 <mandyatwork24@gmail.com>
2023-07-24 10:59:06 -0400

    Handling static dropdown

--- src/HandlingGUI/HandlingStaticDropdown.java
+++ src/HandlingGUI/HandlingStaticDropdown.java
@@ -1,17 +1,46 @@
 package HandlingGUI;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class HandlingStaticDropdown {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\msingh\\Desktop\\Selenium Session\\driver\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://selenium.obsqurazone.com/index.php");
+		driver.get("https://selenium.obsqurazone.com/select-input.php");
+		/**
+		 * In order to select static dropdown there must be a select tag and option tag
+		 * We can use 3 methods to make selection of any element from static dropdown
+		 * 
+		 * selectByIndex()
+		 * selectByValue()
+		 * selectByVisivleText()
+		 */
+		Thread.sleep(2000);
+		WebElement dropdown = driver.findElement(By.xpath("//select[@id='single-input-field']"));
+		
+		Select staticDropdown = new Select(dropdown);
+		
+		staticDropdown.selectByValue("Yellow");
+		Thread.sleep(2000);
+		staticDropdown.selectByIndex(3);
+		Thread.sleep(2000);
+		staticDropdown.selectByVisibleText("Red");
+		
+		
+		List<WebElement> optionsofWeblements= staticDropdown.getOptions();
+		for(WebElement element : optionsofWeblements) {
+			System.out.println("Text present = "+element.getText());
+		}
 
 	}
 
