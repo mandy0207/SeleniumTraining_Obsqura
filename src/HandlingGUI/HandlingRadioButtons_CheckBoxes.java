1c9b840f8d2b067acaea2505f33882a3253bb032
mandy0207 <mandyatwork24@gmail.com>
2023-07-25 11:11:34 -0400

    RadioButtons

---
+++ src/HandlingGUI/HandlingRadioButtons_CheckBoxes.java
@@ -0,0 +1,18 @@
+package HandlingGUI;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandlingRadioButtons_CheckBoxes {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\msingh\\Desktop\\Selenium Session\\driver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://selenium.obsqurazone.com/check-box-demo.php");
+
+	}
+
+}
