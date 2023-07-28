e1923517e603651da30b461d112cf57e926c1184
mandy0207 <mandyatwork24@gmail.com>
2023-07-28 10:57:33 -0400

    Mouse actions

--- src/HandlingGUI/KeyboardMouseActions.java
+++ src/HandlingGUI/KeyboardMouseActions.java
@@ -1,7 +1,11 @@
 package HandlingGUI;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class KeyboardMouseActions {
 
@@ -13,7 +17,24 @@ public class KeyboardMouseActions {
 		driver.manage().window().maximize();
 		driver.get("https://selenium.obsqurazone.com/simple-form-demo.php");
 		
-
+		/**
+		 * whenever we need to handle any element with keyboard and mouse 
+		 * we need to make object of Actions class
+		 */
+		
+		Actions act = new Actions(driver);
+		WebElement others=driver.findElement(By.cssSelector("#others"));
+		/**
+		 * Hover over a webelement
+		 */
+		act.moveToElement(others).build().perform();
+		
+		WebElement textBox = driver.findElement(By.cssSelector("[placeholder='Message']"));
+	
+		//act.moveToElement(textBox).click().sendKeys("jasmine").build().perform();
+		act.moveToElement(textBox).click().keyDown(Keys.SHIFT).sendKeys("misha").build().perform();
+		act.moveToElement(others).build().perform();
+		
 	}
 
 }
