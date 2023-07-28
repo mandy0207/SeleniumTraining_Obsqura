fe485b2deccb015f40e724651c791f0cc19231c5
mandy0207 <mandyatwork24@gmail.com>
2023-07-28 11:26:55 -0400

    ger=neral Methods

--- src/HandlingGUI/GeneralMethods.java
+++ src/HandlingGUI/GeneralMethods.java
@@ -1,6 +1,8 @@
 package HandlingGUI;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GeneralMethods {
@@ -12,6 +14,19 @@ public class GeneralMethods {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://selenium.obsqurazone.com/radio-button-demo.php");
+		WebElement maleRadioBtn = driver.findElement(By.id("inlineRadio1"));
+		
+		System.out.println(maleRadioBtn.isSelected());
+		maleRadioBtn.click();
+		System.out.println(maleRadioBtn.isSelected());
+		
+		
+		WebElement showSelectedValueBtn = driver.findElement(By.xpath("//*[@id='button-one']"));
+		System.out.println("Is it displayed : "+showSelectedValueBtn.isDisplayed());
+		
+		System.out.println("AM I ENABLED :"+showSelectedValueBtn.isEnabled());
+		
+		driver.quit();
 
 	}
 
