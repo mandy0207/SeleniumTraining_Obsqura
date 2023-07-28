1fad3e62fee1e388fd77e040be50e9dfd1daec6c
mandy0207 <mandyatwork24@gmail.com>
2023-07-28 11:19:34 -0400

    ger=neral Methods

---
+++ src/HandlingGUI/GeneralMethods.java
@@ -0,0 +1,18 @@
+package HandlingGUI;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GeneralMethods {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\msingh\\Desktop\\Selenium Session\\driver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://selenium.obsqurazone.com/radio-button-demo.php");
+
+	}
+
+}
