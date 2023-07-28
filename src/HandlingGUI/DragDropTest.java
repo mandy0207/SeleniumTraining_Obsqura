819e49152fd9576632bcb607c3d0ac11f8fc57b5
mandy0207 <mandyatwork24@gmail.com>
2023-07-28 11:07:31 -0400

    Drag Drop

---
+++ src/HandlingGUI/DragDropTest.java
@@ -0,0 +1,18 @@
+package HandlingGUI;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DragDropTest {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\msingh\\Desktop\\Selenium Session\\driver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demoqa.com/droppable");
+
+	}
+
+}
