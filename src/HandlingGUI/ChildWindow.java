7dfefecc356e16ea562ab493afbcaec426f2362c
mandy0207 <mandyatwork24@gmail.com>
2023-07-31 11:05:21 -0400

    ChildWindow

--- src/HandlingGUI/ChildWindow.java
+++ src/HandlingGUI/ChildWindow.java
@@ -1,5 +1,9 @@
 package HandlingGUI;
 
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -14,7 +18,28 @@ public class ChildWindow {
 		driver.manage().window().maximize();
 		driver.get("https://the-internet.herokuapp.com/windows");
 		
+		
+		System.out.println(driver.getTitle());
+		
+		driver.findElement(By.cssSelector(".example a")).click();
+		
+		Set<String> windows = driver.getWindowHandles();
+		
+		Iterator<String> it = windows.iterator();
 
+		String parentWindow = it.next();
+		String childWindow= it.next();
+		
+		driver.switchTo().window(childWindow);
+		
+		System.out.println(driver.getTitle());
+		String TextPresent = driver.findElement(By.xpath("//h3")).getText();
+		System.out.println("Text Present on page="+TextPresent);
+		
+		driver.switchTo().window(parentWindow);
+		
+		System.out.println(driver.getTitle());
+		
 	}
 
 }
