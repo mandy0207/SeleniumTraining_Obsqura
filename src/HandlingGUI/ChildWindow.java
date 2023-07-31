9d01ba2ef90ebdb2c9678b868a2245f68225019f
mandy0207 <mandyatwork24@gmail.com>
2023-07-31 10:39:23 -0400

    ChildWindow

---
+++ src/HandlingGUI/ChildWindow.java
@@ -0,0 +1,20 @@
+package HandlingGUI;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChildWindow {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\msingh\\Desktop\\Selenium Session\\driver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://the-internet.herokuapp.com/windows");
+		
+
+	}
+
+}
