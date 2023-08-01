ef1d5b480fbe31ca9255cd6def3a3ef36b0a2c0d
mandy0207 <mandyatwork24@gmail.com>
2023-08-01 11:36:28 -0400

    Tables

--- src/HandlingGUI/HandlingTables.java
+++ src/HandlingGUI/HandlingTables.java
@@ -1,6 +1,10 @@
 package HandlingGUI;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HandlingTables {
@@ -14,7 +18,16 @@ public class HandlingTables {
 		driver.manage().window().maximize();
 		driver.get("https://selenium.obsqurazone.com/table-pagination.php");
 		
-
+		List<WebElement> list = driver.findElements(By.xpath("//table[@id='dtBasicExample']//tr[3]//td"));
+		System.out.println(list.size());
+		
+		for(int i =0;i<list.size();i++) {
+			
+			String textofTD = list.get(i).getText();
+			System.out.println(textofTD);
+		}
+		
+		driver.quit();
 	}
 
 }
