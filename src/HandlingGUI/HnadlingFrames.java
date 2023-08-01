53ed9a5539d33b0b53febbee199baafd004d53b3
mandy0207 <mandyatwork24@gmail.com>
2023-08-01 11:02:56 -0400

    Frames

--- src/HandlingGUI/HnadlingFrames.java
+++ src/HandlingGUI/HnadlingFrames.java
@@ -1,6 +1,8 @@
 package HandlingGUI;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HnadlingFrames {
@@ -12,6 +14,27 @@ public class HnadlingFrames {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://the-internet.herokuapp.com/iframe");
+		String heading = driver.findElement(By.xpath("//h3")).getText();
+		System.out.println(heading);
+		/**
+		 * 1)When ever any element is present inside frame. We have to switch inside frame to perform operations
+		 * 2) We can switch inside frame by three ways
+		 *  using id or name of frame, using index of frame or using webelement of frame
+		 */
+		
+		
+		driver.switchTo().frame("mce_0_ifr");
+		
+		WebElement msgBox=driver.findElement(By.cssSelector("[id='tinymce'] "));
+		msgBox.clear();
+		msgBox.sendKeys("I am misha");
+		/**
+		 * With following statement driver will come out of frame
+		 */
+		driver.switchTo().defaultContent();
+		System.out.println(driver.findElement(By.xpath("//h3")).getText());
+		
+		driver.quit();
 
 	}
 
