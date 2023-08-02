06747f1a9a00b817cb00c3a4b33a20623b6fb34c
mandy0207 <mandyatwork24@gmail.com>
2023-08-02 11:28:41 -0400

    Selenium Waits

--- src/HandlingGUI/Learning_Waits.java
+++ src/HandlingGUI/Learning_Waits.java
@@ -1,18 +1,62 @@
 package HandlingGUI;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Learning_Waits {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\msingh\\Desktop\\Selenium Session\\driver\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		
 		driver.get("https://selenium.obsqurazone.com/dynamic-load.php");
+	
+		driver.findElement(By.xpath("//*[@id='save']")).click();
+		String Name=driver.findElement(By.xpath("//h5")).getText();
+	
+		System.out.println(Name);
+		
+		/**
+		 * Implicit wait applicable globally
+		 * 
+		 */
+		
+		driver.findElement(By.xpath("//a[@id='progress-bars']")).click();
+		driver.findElement(By.xpath("//*[@id='downloadButton']")).click();
+		
+
+	
+    WebElement progressBar=driver.findElement(By.cssSelector("div[class='progress-label']"));
+
+
+		/**
+		 * Explicit Wait
+		 */
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+	
+		
+		wait.until(ExpectedConditions.textToBePresentInElement(progressBar, "Complete!"));
+		
+     	WebElement closeBtn = driver.findElement(By.cssSelector("button[class='ui-button ui-corner-all ui-widget']"));
+     	System.out.println(closeBtn.getText());
+
+     	closeBtn.click();
+		
+	
+		
+		
+
 
 
 	}
